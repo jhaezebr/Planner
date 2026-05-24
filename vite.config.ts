f963d1dc415e3827a4ed8e5c841c41aea2bf682e
Jules Haezebrouck <jules.haezebrouck@ugent.be>
2026-05-24 16:52:12 +0200

    singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   base: './',
   test: {
     environment: 'node',
